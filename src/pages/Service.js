e1776cf3fb36ee4809d39258c5053cc8ad877d57
diegodl2002 <diegodl20022@gmail.com>
2022-04-26 19:45:42 -0300

    added form service

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -1,9 +1,29 @@
-import NewProduct from "../Components/NewProduct.js";
+/* import NewProduct from "../Components/NewProduct.js"; */
 const Service = () => {
-    return <h1>Services </h1>;
-    <div>
-      <NewProduct />
-    </div>
+    return (
+      <div>
+        <h1> Service Formulario de Ingreso</h1>
+        <form>
+        <label>Nombre Cliente:  </label>
+        <input type="text" />
+        <label>Fecha:  </label>
+        <input type="text" />
+        <label>Equipo:  </label>
+        <input type="text" />
+        <hr />
+        <label>Falla:  </label>
+        <input type="text" />
+        <label>Detalles:  </label>
+        <input type="text" />
+        <label>Observaciones:  </label>
+        <input type="text" />
+        <hr />
+        <label>Estado Orden:  </label>
+        <input type="text" />
+        </form>
+      </div>
+    )
+    
 
   };
   
